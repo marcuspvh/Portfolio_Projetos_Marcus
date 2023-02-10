776c8853f887c36ce7062b903c9dad8bf0e85e07
Marcus <marcuspvh@gmail.com>
2023-02-09 20:12:16 -0400

    Portfolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Marcus Roberto Ribeiro",
+  githubUser: "marcuspvh",
+  linkedinUser: "marcus-roberto",
+  whatsappNumber: "+5569981147755",
+  emailUser: "marcuspvh@gmail.com.br",
+  instagramUser: "marcuspvh",
   facebookUser: "",
 };
